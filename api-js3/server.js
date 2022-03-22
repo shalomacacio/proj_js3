133c66db18f4ac6c40d2e6db0760a9045ed10664
Shalom Acacio <shalom.acacio@gmail.com>
2022-03-21 23:30:25 -0300

    removendo api node

--- api-js3/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const port = 3000;
-const app = require('./app')
-const server = http.createServer(app)
-
-app.listen(port, () => {
-    console.log('Servidor rodando na porta '+ port)
-});
